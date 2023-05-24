6802a1f244687e7423fd90258f1e73bebb44aa50
unknown <giiozera24@gmail.com>
2023-05-24 17:58:42 -0400

    construindo modal

--- lib/widget/modal_cardapio.dart
+++ lib/widget/modal_cardapio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stretchy_header/stretchy_header.dart';
 
 class ModalCardapio extends StatelessWidget {
   final String titulo;
@@ -14,19 +15,24 @@ class ModalCardapio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.95,
-      width: MediaQuery.of(context).size.width * 1,
-      child: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.5,
-            width: MediaQuery.of(context).size.width * 0.7,
-            child: Image.network('https://api.gdelivery.app.br/files/' + img,
-                fit: BoxFit.contain),
-          )
-        ],
-      ),
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.90,
+      child: StretchyHeader.singleChild(
+          headerData: HeaderData(
+              header: Image.network('https://api.gdelivery.app.br/files/$img'),
+              headerHeight: MediaQuery.of(context).size.height * 0.30),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 500,
+                child: Text('teste'),
+              ),
+              SizedBox(
+                height: 500,
+                child: Text('teste'),
+              )
+            ],
+          )),
     );
   }
 }
