9d2b30aa05e0a6afcf757de6595275139988c8e4
xofaa <giovanne.oliv@gmail.com>
2023-05-30 11:00:09 -0300

    estilizando modal cardapio

--- lib/widget/modal_cardapio.dart
+++ lib/widget/modal_cardapio.dart
@@ -1,101 +1,148 @@
 import 'package:flutter/material.dart';
-import 'package:stretchy_header/stretchy_header.dart';
 
-class ModalCardapio extends StatelessWidget {
+class ModalCardapio extends StatefulWidget {
   final String titulo;
   final String subtitulo;
   final String valor;
   final String img;
-  const ModalCardapio(
-      {super.key,
-      required this.titulo,
-      required this.subtitulo,
-      required this.valor,
-      required this.img});
+
+  const ModalCardapio({
+    Key? key,
+    required this.titulo,
+    required this.subtitulo,
+    required this.valor,
+    required this.img,
+  }) : super(key: key);
+
+  @override
+  State<ModalCardapio> createState() => _ModalCardapioState();
+}
+
+class _ModalCardapioState extends State<ModalCardapio> {
+  bool isKeyboard = false;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.90,
+      height: MediaQuery.of(context).size.height * 0.9,
       child: Scaffold(
         resizeToAvoidBottomInset: true,
-        body: StretchyHeader.singleChild(
-            headerData: HeaderData(
-                blurContent: false,
-                header: Container(
-                    margin: const EdgeInsets.only(top: 10),
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.90,
-                      child: Image.network(
-                          'https://api.gdelivery.app.br/files/$img'),
-                    )),
-                headerHeight: MediaQuery.of(context).size.height * 0.30),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.10,
+        body: Stack(
+          children: [
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  leading: IconButton(
+                    icon: Icon(Icons.arrow_drop_down_circle_rounded),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
-                  Text(
-                    titulo,
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold),
+                  expandedHeight: 300,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.network(
+                      'https://api.gdelivery.app.br/files/${widget.img}',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.10,
+                            ),
+                            Text(
+                              widget.titulo,
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.01,
+                            ),
+                            Text(
+                              widget.subtitulo,
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.01,
+                            ),
+                            Text(
+                              'R\$' + widget.valor,
+                              style: TextStyle(
+                                  fontSize: 20, color: Colors.green[700]),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.03,
+                            ),
+                            const Row(
+                              children: [
+                                Icon(Icons.info),
+                                Text('Alguma observação ?')
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.02,
+                            ),
+                            const TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText:
+                                    "ex: Tirar cebola, acrescentar maionese.",
+                              ),
+                              maxLength: 50,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    subtitulo,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  Text(
-                    // ignore: prefer_interpolation_to_compose_strings
-                    'R\$' + valor,
-                    style: TextStyle(fontSize: 20, color: Colors.green[700]),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.03,
-                  ),
-                  const Row(
-                    children: [Icon(Icons.info), Text('Alguma observação ?')],
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.02,
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "ex: Tirar cebola, acrescentar maionese."),
-                    maxLength: 50,
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  Row(
+                ),
+              ],
+            ),
+            Visibility(
+              visible: !isKeyboard,
+              child: Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Row(
                         children: [
                           IconButton(
-                              onPressed: () {}, icon: Icon(Icons.remove)),
+                            onPressed: () {},
+                            icon: Icon(Icons.remove),
+                          ),
                           const SizedBox(
                             width: 10,
                             child: Text('2'),
                           ),
-                          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.add),
+                          ),
                         ],
                       ),
-                      ElevatedButton(onPressed: () {}, child: Text('adicionar'))
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('adicionar'),
+                      ),
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            )),
+            ),
+          ],
+        ),
       ),
     );
   }
